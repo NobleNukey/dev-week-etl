aa9cdcdee96a98368603d4221c502d28c9f5eead
NobleNukey <jcarlosvsantos@outlook.com>
2026-03-16 18:18:06 -0300

    Add files via upload

---
+++ meu_desafio.py
@@ -0,0 +1,65 @@
+# Nossa lista de utilizadores com saldo positivo (Extração)
+clientes = [
+    {
+        "name": "Jean Engenheiro",
+        "account": {
+            "number": "10001-1",
+            "agency": "0001",
+            "balance": 5200.00,
+            "limit": 2000.00},
+        "card": {
+            "number": "**** **** **** 9999",
+            "limit": 5000.0},
+        "news": []
+    },
+    {
+        "name": "Aline Tech",
+        "account": {
+            "number": "10002-2",
+            "agency": "0001",
+            "balance": 850.00,
+            "limit": 1000.00},
+        "card": {
+            "number": "**** **** **** 8888",
+            "limit": 2000.0},
+        "news": []
+    },
+    {
+        "name": "Bruno Python",
+        "account": {
+            "number": "10003-3",
+            "agency": "0001",
+            "balance": 120.00,
+            "limit": 500.00},
+        "card": {
+            "number": "**** **** **** 7777",
+            "limit": 500.0},
+        "news": []
+    },
+    {
+        "name": "Carla Dados",
+        "account": {
+            "number": "10004-4",
+            "agency": "0001",
+            "balance": 15000.00,
+            "limit": 10000.00},
+        "card": {
+            "number": "**** **** **** 6666",
+            "limit": 15000.0},
+        "news": []
+    },
+    {
+        "name": "Diego Dev",
+        "account": {
+            "number": "10005-5",
+            "agency": "0001",
+            "balance": 2500.00,
+            "limit": 300.00},
+        "card": {
+            "number": "**** **** **** 5555",
+            "limit": 1000.0},
+        "news": []
+    }
+]
+
+print("✅ Passo 1: Clientes 'extraídos' com sucesso!")
